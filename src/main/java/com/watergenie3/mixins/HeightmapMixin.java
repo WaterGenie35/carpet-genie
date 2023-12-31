2ec938dd937f18e2afcbcfcf545649ab26845707
WaterGenie35 <thirdwatergenie@gmail.com>
2023-12-31 19:41:29 +0700

    Remove redundant shadow

--- src/main/java/com/watergenie3/mixins/HeightmapMixin.java
+++ src/main/java/com/watergenie3/mixins/HeightmapMixin.java
@@ -1,26 +1,28 @@
 package com.watergenie3.mixins;
 
 import java.util.Set;
+import java.util.function.Predicate;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.chunk.Chunk;
 
 @Mixin(Heightmap.class)
 public abstract class HeightmapMixin {
 
-    @Shadow public static void populateHeightmaps(Chunk chunk, Set<Heightmap.Type> types) {}
+    // don't need this for chunkmixin
+    // @Shadow public static void populateHeightmaps(Chunk chunk, Set<Heightmap.Type> types) {}
 
-    // @Shadow @Final static Predicate<BlockState> NOT_AIR;
     // @Shadow @Final static Predicate<BlockState> NOT_AIR_NOR_BEDROCK;
     // @Unique static final Predicate<BlockState> NOT_AIR_NOR_BEDROCK = state -> !state.isAir() && state.getBlock() != Blocks.BEDROCK;
 
     // Predicate<BlockState> NOT_AIR_NOR_BEDROCK() {
     //     return state -> !state.isAir() && state.getBlock() != Blocks.BEDROCK;
     // }
-
-    
 
 }
