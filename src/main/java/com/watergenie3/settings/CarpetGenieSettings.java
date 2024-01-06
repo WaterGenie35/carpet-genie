21c4b196f7c20a2facc154cf1587f9d93d2ff9ff
WaterGenie35 <thirdwatergenie@gmail.com>
2024-01-06 15:43:07 +0700

    Add some ideas for sign/banner mte alternatives and repeater odd tick and amplifier

--- src/main/java/com/watergenie3/settings/CarpetGenieSettings.java
+++ src/main/java/com/watergenie3/settings/CarpetGenieSettings.java
@@ -10,6 +10,8 @@ public class CarpetGenieSettings {
 
     public static final String SPAWNING = "spawning";
     public static final String ANVIL = "anvil";
+    public static final String MTE = "mte";
+    public static final String REDSTONE = "redstone";
     public static final String CLIENT = "client";
 
     @Rule(categories = {CARPETGENIE, FEATURE, SPAWNING})
@@ -33,6 +35,15 @@ public class CarpetGenieSettings {
 
     @Rule(categories = {CARPETGENIE, FEATURE, SURVIVAL, ANVIL})
     public static boolean upgradingEnchantmentsDoesNotIncreaseAnvilUse = false;
+
+    @Rule(categories = {CARPETGENIE, FEATURE, MTE})
+    public static boolean immovableNetherWoodSignBanner = false;
+
+    @Rule(categories = {CARPETGENIE, FEATURE, REDSTONE})
+    public static boolean repeaterOnBlueIceHas1gtLessDelay = false;
+
+    @Rule(categories = {CARPETGENIE, FEATURE, REDSTONE})
+    public static boolean repeaterOnMagentaGlazedTerracottaPowersExtraBlock = false;
 
     @Rule(categories = {CARPETGENIE, FEATURE, CLIENT})
     public static boolean highlightTargetFloor = false;
